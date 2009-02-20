e6fdc5b9776ee4c303f98bf8ae23e21cd63bb1ea
Eric Butler <eric@extremeboredom.net>
2009-02-19 18:41:08 -0800

    Allow embeded images. This is still not good enough though, because attribute order is enforced...

--- src/Synapse.Core/Synapse.Core/HtmlSanitizer.cs
+++ src/Synapse.Core/Synapse.Core/HtmlSanitizer.cs
@@ -12,12 +12,12 @@ namespace Synapse.Core
 	{
 		private static Regex _tags = new Regex("<[^>]*(>|$)", RegexOptions.Singleline | RegexOptions.ExplicitCapture | RegexOptions.Compiled);
 		private static Regex _whitelist = new Regex(@"
-		    ^</?(a|b(lockquote)?|code|em|h(1|2|3)|i|li|ol|p(re)?|s(ub|up|trong|trike)?|ul)>$
-		    |^<(b|h)r\s?/?>$
-		    |^<a[^>]+>$
-		    |^<img[^>]+/?>$",
-		    RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace |
-		    RegexOptions.ExplicitCapture | RegexOptions.Compiled);
+			^</?(a|b(lockquote)?|code|em|h(1|2|3)|i|li|ol|p(re)?|s(ub|up|trong|trike)?|ul)>$
+			|^<(b|h)r\s?/?>$
+			|^<a[^>]+>$
+			|^<img[^>]+/?>$",
+			RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace |
+			RegexOptions.ExplicitCapture | RegexOptions.Compiled);
 		
 		/// <summary>
 		/// sanitize any potentially dangerous tags from the provided raw HTML input using 
@@ -26,52 +26,52 @@ namespace Synapse.Core
 		public static string Sanitize(string html)
 		{
 		
-		    var tagname = "";
-		    Match tag;
-		    var tags = _tags.Matches(html);
+			var tagname = "";
+			Match tag;
+			var tags = _tags.Matches(html);
 		
-		    // iterate through all HTML tags in the input
-		    for (int i = tags.Count-1; i > -1; i--)
-		    {
-		        tag = tags[i];
-		        tagname = tag.Value.ToLower();
+			// iterate through all HTML tags in the input
+			for (int i = tags.Count-1; i > -1; i--)
+			{
+				tag = tags[i];
+				tagname = tag.Value.ToLower();
 		
-		        if (!_whitelist.IsMatch(tagname))
-		        {
-		            // not on our whitelist? I SAY GOOD DAY TO YOU, SIR. GOOD DAY!
-		            html = html.Remove(tag.Index, tag.Length);
-		        }
-		        else if (tagname.StartsWith("<a"))
-		        {
-		            // detailed <a> tag checking
+				if (!_whitelist.IsMatch(tagname))
+				{
+					// not on our whitelist? I SAY GOOD DAY TO YOU, SIR. GOOD DAY!
+					html = html.Remove(tag.Index, tag.Length);
+				}
+				else if (tagname.StartsWith("<a"))
+				{
+					// detailed <a> tag checking
 					// FIXME: allow xmpp: uris!
-		            if (!IsMatch(tagname,
-		                @"<a\s
-		                  href=""(\#\d+|(https?|ftp)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+)""
-		                  (\stitle=""[^""]+"")?\s?>"))
-		            {
-		                html = html.Remove(tag.Index, tag.Length);
-		            }
-		        }
-		        else if (tagname.StartsWith("<img"))
-		        {
-		            // detailed <img> tag checking
-		            if (!IsMatch(tagname,
-		                @"<img\s
-		              src=""https?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+""
-		              (\swidth=""\d{1,3}"")?
-		              (\sheight=""\d{1,3}"")?
-		              (\salt=""[^""]*"")?
-		              (\stitle=""[^""]*"")?
-		              \s?/?>"))
-		            {
-		                html = html.Remove(tag.Index, tag.Length);
-		            }
-		        }
-		        
-		    }
+					if (!IsMatch(tagname,
+						@"<a\s
+						  href=""(\#\d+|(https?|ftp)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+)""
+						  (\stitle=""[^""]+"")?\s?>"))
+					{
+						html = html.Remove(tag.Index, tag.Length);
+					}
+				}
+				else if (tagname.StartsWith("<img"))
+				{
+					// detailed <img> tag checking
+					if (!IsMatch(tagname,
+					  @"<img\s
+					  src=""(data:image/png;base64,[0-9a-zA-Z\+/=]+|https?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+)""
+					  (\swidth=""\d{1,3}"")?
+					  (\sheight=""\d{1,3}"")?
+					  (\salt=""[^""]*"")?
+					  (\stitle=""[^""]*"")?
+					  \s?/?>"))
+					{
+						html = html.Remove(tag.Index, tag.Length);
+					}
+				}
+				
+			}
 		
-		    return html;
+			return html;
 		}
 				
 		/// <summary>
@@ -79,8 +79,8 @@ namespace Synapse.Core
 		/// </summary>
 		private static bool IsMatch(string s, string pattern)
 		{
-		    return Regex.IsMatch(s, pattern, RegexOptions.Singleline | RegexOptions.IgnoreCase |
-		        RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture);
+			return Regex.IsMatch(s, pattern, RegexOptions.Singleline | RegexOptions.IgnoreCase |
+				RegexOptions.IgnorePatternWhitespace | RegexOptions.ExplicitCapture);
 		}		
 	}
 }
